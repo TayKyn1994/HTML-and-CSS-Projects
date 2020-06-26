c9483453e45e891886842d509acc7ecd8d646e62
TayKyn1994 <66887167+TayKyn1994@users.noreply.github.com>
2020-06-26 11:08:08 -0700

    Add files via upload

---
+++ Basic_HTML_and_JavaScript_1/basic_javascript_1.js
@@ -0,0 +1,8 @@
+alert("Hello, World!");
+
+function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
